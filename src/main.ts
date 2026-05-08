5d4710ed1af757e3baac09b016cc32039c073ba9
Guido Pujadas <guidopujadas@gmail.com>
2026-05-08 11:10:19 -0300

    generic configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+// sin estos DNS la conexión con mongo atlas no me funciona
+import dns from 'dns';
+dns.setServers(['1.1.1.1', '8.8.8.8']);   // ← Force reliable DNS (Cloudflare + Google)
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Weather API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
+
 bootstrap();
